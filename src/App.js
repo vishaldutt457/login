56cdb4f50670877acc6a82f5500f1bf4f0df0ba9
vishaldutt457 <duttvishal689@gmail.com>
2021-08-18 14:37:24 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import { auth, GoogleAuth,  FacebookAuth} from './firebase';
+import {Button} from "@material-ui/core"
+
 
 function App() {
+  
+  const signIn = () => {
+      auth.signInWithPopup(FacebookAuth)
+  }
+    
+    const login = () => {
+      auth.signInWithPopup(GoogleAuth)
+    }
+
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div>
+     <Button onClick={signIn} variant="contained" color="primary">Facebook</Button>
+    <Button onClick={login} variant="contained" color="primary">Google</Button>
+   </div>     
+    )
 }
 
-export default App;
+export default App
